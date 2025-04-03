61927da12560be2ef6c9f864eefccf80ea27e9b5
Batin Adiguzel <batinadiguzel7@gmail.com>
2025-04-03 11:33:19 +0200

    python_variables.py

---
+++ python_variables.py
@@ -0,0 +1,19 @@
+#Create a variable
+x= "Batın"
+y = 2005
+print(x)
+print(y)
+#Variable names 
+my_var = "Batın"
+print(my_var)
+#Assign multiple values
+x,y,z = ("volvo","audi","honda")
+print(x)
+print(y)
+print(z)
+#Unpack a collection
+carsmodels = ["s90","rs6","typeR"]
+x, y, z = carsmodels
+print(x)
+print(y)
+print(z)
